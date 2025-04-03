e80927a73841f2e9afcd73411b8b0214174ebdde
unknown <wlinke15@gmail.com>
2025-04-03 09:49:54 -0400

    Updates

---
+++ precache-manifest.31014ac7f632ffa36ff66b93afece9a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64609f8e0ceb6b5120b9d934d0c8706",
+    "url": "/RecipeVault/index.html"
+  },
+  {
+    "revision": "877d18e321321f8c02bb",
+    "url": "/RecipeVault/static/css/main.62db5af8.chunk.css"
+  },
+  {
+    "revision": "2381880b0b4bcc0ad9aa",
+    "url": "/RecipeVault/static/js/2.b558a5d0.chunk.js"
+  },
+  {
+    "revision": "5735eaba45af69b5ce39025a9d6a4f72",
+    "url": "/RecipeVault/static/js/2.b558a5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83fffb2049257ab38d1",
+    "url": "/RecipeVault/static/js/3.ac72ee45.chunk.js"
+  },
+  {
+    "revision": "877d18e321321f8c02bb",
+    "url": "/RecipeVault/static/js/main.a5dfd8ae.chunk.js"
+  },
+  {
+    "revision": "defe63f37e60212cc1dc",
+    "url": "/RecipeVault/static/js/runtime-main.d0c28230.js"
+  }
+]);
